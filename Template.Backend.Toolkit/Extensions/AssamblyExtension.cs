e70ad4ec25acc54586a0d84d0672d2104c512469
shabanov_iv <limsdev@mail.ru>
2023-12-15 12:56:25 +0500

    Выполните обновление пакетов

---
+++ Template.Backend.Toolkit/Extensions/AssamblyExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace Template.Backend.Toolkit.Extensions;
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Расширения для Reflection, сканирование типов и сборок.
+/// </summary>
+public static class AssamblyExtension
+{
+    private static List<Type> GetAllBaseTypes(this Type type)
+    {
+        var baseTypes = new List<Type>();
+
+        if (type.BaseType != null)
+        {
+            baseTypes.Add(type.BaseType);
+            baseTypes.AddRange(type.BaseType.GetAllBaseTypes());
+        }
+        return baseTypes;
+    }
+}
